90bf2f2cc257845f24c5f1aa8fd33edaef43e2fd
lpdft <772346730@qq.com>
2020-09-10 10:56:50 +0800

    [ruyue] feat:add function pass the test

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,41 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class GrammarExercise {
     public static void main(String[] args) {
         //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+        Scanner scanner = new Scanner(System.in);
+        String firstWordList = scanner.nextLine();
+        String secondWordList = scanner.nextLine();
 
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
         //按要求输出到命令行
-
+        System.out.println(result);
     }
 
-    public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
+    public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) throws RuntimeException{
         //在这编写实现代码
-        return null;
+        List<String> validFirstWordList = isValid(firstWordList);
+        List<String> validSecondWordList = isValid(secondWordList);
+        return validFirstWordList.stream()
+                .filter(validSecondWordList::contains)
+                .distinct()
+                .map(s -> String.join(" ", s.split("")))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> isValid(String input) throws RuntimeException{
+        if(input.contains(",,")) {
+            throw new RuntimeException("input not valid");
+        }
+        return Arrays.stream(input.split(",")).map(s -> {
+            if(s.matches("[a-zA-Z]+")) {
+                return s.toUpperCase();
+            } else {
+                throw new RuntimeException("input not valid");
+            }
+        }).collect(Collectors.toList());
     }
 }
